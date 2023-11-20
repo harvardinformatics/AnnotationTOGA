685fa1a3665b48e11659311ef5ff345d3fa4bb54
afreedman <adamfreedman@fas.harvard.edu>
2023-11-20 11:42:53 -0500

    added and simplified toga bed filter requiring presence in ortholog table

---
+++ utilities/FilterTogaAnnotationBedFile.py
@@ -0,0 +1,24 @@
+import argparse
+
+if __name__=="__main__": 
+    parser = argparse.ArgumentParser(description="filter TOGA annotation bed file so only keeps classified orthologs")
+    parser.add_argument('-orthtable','--orthology-classification-table',dest='orthos',type=str,help='orthology classification toga file')
+    parser.add_argument('-bedin','--query-annotation-bed',dest='annotbed',type=str,help='query_annotation.bed toga file')
+    opts = parser.parse_args()
+
+    orthology = open(opts.orthos,'r')
+    orthology.readline()
+    orthoset = set()
+    for line in orthology:
+        linelist = line.strip().split('\t')
+        orthoset.add(linelist[3])
+
+    no_ortho_counter = 0
+    bedopen = open(opts.annotbed,'r')
+    bedout = open('filteredtest_%s' % opts.annotbed,'w')
+    for line in bedopen:
+        linelist = line.strip().split('\t')
+        if linelist[3] in orthoset:
+            bedout.write(line)
+
+    bedout.close()
